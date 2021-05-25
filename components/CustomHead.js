6ccb2146bc63800eac1e5f6cfc5473e60ec883dd
Eikix <66871571+Eikix@users.noreply.github.com>
2021-05-25 15:55:59 +0200

    sixth commit

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -6,7 +6,6 @@ function CustomHead({title, description}) {
             <Head>
                 <title>{title}</title>
                 <meta charSet="utf-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="description" content={description} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
