968cb558ec789cf02c6a04cab9e790f81e545830
Eikix <66871571+Eikix@users.noreply.github.com>
2021-05-25 16:10:37 +0200

    sixth commit

--- components/Services.js
+++ components/Services.js
@@ -4,7 +4,7 @@ function Services({servicesData}) {
     return (
         <div className="flex flex-col items-center">
             <h1 className="mt-12 border-b text-3xl p-3 text-gray-700">Gestes Chirurgicaux</h1>
-            <div className="flex flex-col md:grid md:gap-6 md:grid-cols-3 lg:grid-cols-4 space-y-6 md:space-y-0 m-6 p-6 justify-center text-center border rounded-lg shadow-sm">
+            <div className="grid grid-cols-1 gap-3 md:gap-6 md:grid-cols-3 lg:grid-cols-4 m-6 p-6 justify-center text-center border rounded-lg shadow-sm">
                 {servicesData.map( service => <ServicesItem title={service.title} description={service.description} />)}
             
             </div>
